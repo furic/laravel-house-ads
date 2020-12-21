09bb15d6fe614429a8628af23449398f9a32c41b
Richard Fu <fur@richardfu.net>
2020-12-21 14:28:08 +1100

    Fix missing close blanklet in last commit.

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Furic\HouseAds\Http\Controllers\HouseAdController;
 
-Route::prefix('api')->group(function () {
+Route::prefix('api')->group(function() {
 
 	Route::resource('house-ads', HouseAdController::class)->only([
 	    'index', 'show', 'update'
 	]);
 
-}
+});
